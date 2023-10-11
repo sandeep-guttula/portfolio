c2b64110f541432e54a287182a25b263276b36cc
sandeep-guttula <sandeepguttula2002@gmail.com>
2023-10-11 13:33:37 +0530

    home updated

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,32 +1,59 @@
 // import bgsvg from "../assets/backgroundBlurs.svg";
 const Home = () => {
   const angle = ">";
-  const text = "// find my profile on Github:";
+  const text = [
+    "// complete the game to continue",
+    "// you can also see it on my Github page",
+    "// find my profile on Github:",
+  ];
+
   return (
     <div className="font-fira h-[92%]">
       {/* <img src={bgsvg} alt="" className="fill-[#43D9AD] blur-sm opacity-40 " /> */}
       <div className="h-full">
-        <div className="h-full left flex flex-col justify-evenly items-centers w-full px-5">
-          <div>
-            <span className="text-textWhite font-normal text-base">
-              Hi all. I am
-            </span>
-            <h1 className="text-textWhite font-normal text-6xl">
-              Sandeep Guttula
-            </h1>
-            <span className="text-textGreen font-normal text-xl">
-              {angle} Front-end developer
-            </span>
-          </div>
-          <div>
-            <p className="text-textSecondary  text-xs">{text}</p>
-            <p>
-              <span className="text-textBlue text-[12px]">const </span>
-              <span className="text-textGreen text-[12px]">githubLink </span>
-              <span className="text-textBrown  text-[12px]">
-                = “https://github.com/sandeep-guttula”
+        <div className="left h-full flex flex-col justify-evenly items-center w-full lg:w-[50%] ">
+          <div className=" h-full flex flex-col justify-evenly items-center">
+            <div className=" w-max flex flex-col justify-center">
+              <span className="text-textWhite font-normal text-base">
+                Hi all. I am
               </span>
-            </p>
+              <h1 className="text-textWhite font-normal text-6xl">
+                Sandeep{" "}
+                <span className="lg:hidden">
+                  <br />
+                </span>{" "}
+                Guttula
+              </h1>
+              <span className="text-textGreen font-normal text-xl">
+                {angle} Front-end developer
+              </span>
+            </div>
+            <div className="w-full">
+              <div className="w-3/4 pl-16 lg:pl-0 ">
+                <p className="hidden lg:flex text-textSecondary  text-xs">
+                  {text[0]}
+                </p>
+                <p className="hidden lg:flex text-textSecondary  text-xs">
+                  {text[1]}
+                </p>
+                <p className="lg:hidden text-textSecondary  text-xs">
+                  {text[2]}
+                </p>
+                <a
+                  href="https://github.com/sandeep-guttula"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <span className="text-textBlue text-[12px]">const </span>
+                  <span className="text-textGreen text-[12px]">
+                    githubLink{" "}
+                  </span>
+                  <span className="text-textBrown  text-[12px]">
+                    = “https://github.com/sandeep-guttula”
+                  </span>
+                </a>
+              </div>
+            </div>
           </div>
         </div>
         <div className="hidden lg:flex w-full bg-white"></div>
